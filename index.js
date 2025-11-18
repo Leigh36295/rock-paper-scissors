4980b5b2547c986a18e1d39a53865b0c75993437
Leigh36295 <leigh36295@gmail.com>
2025-11-18 17:12:23 +0000

    created human choice logic

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ let computerChoice = (Math.random());
 }
 
 console.log(getComputerChoice());
+
+/*creates a prompt allowing user input*/
+function getHumanChoice() {
+    const choice = (window.prompt("Choose your weapon!"));
+    if (choice == "rock") {
+        return "you picked rock";
+    } else if (choice == "paper"){
+        return "you picked paper";
+    } else if (choice == "scissors") {
+        return "you picked scissors"
+    }
+}
+
+    console.log(getHumanChoice());
+    
